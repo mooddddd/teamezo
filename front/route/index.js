43954a9d06feb3ef1152744251603c35637eb7cc
mood <chamesh@naver.com>
2023-01-31 17:07:09 +0900

    feat : listForm.html 만들기

--- front/route/index.js
+++ front/route/index.js
@@ -1,8 +1,9 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index.html");
+    res.render("layout/listForm.html");
 });
 
 module.exports = router;
