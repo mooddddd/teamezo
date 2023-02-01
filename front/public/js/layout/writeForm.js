9e9b1058ac6ea111f3724251cba3e84b88ea0405
mood <chamesh@naver.com>
2023-02-01 09:02:05 +0900

    feat:writeForm.htm, css 만들기 완료(해쉬태그 미완)

---
+++ front/public/js/layout/writeForm.js
@@ -0,0 +1,17 @@
+// const addTagBtn = document.querySelector("#addTagBtn");
+// const input = document.querySelector("#addValue");
+// const ul = document.querySelector("hashtag");
+
+// const handler = (e) => {
+//   e.preventDefault();
+//   let hashtag = document.getElementById("addValue").value;
+//   if (hashtag === "") alert("해시태그를 입력해주세요");
+//   const li = document.createElement("li");
+//   li.setAttribute("name", "tagName");
+//   const tag = document.createTextNode(hashtag);
+//   li.appendChild(tag);
+//   document.getElementById("hashtag").appendChild(li);
+//   input.value = "";
+// };
+
+// addTagBtn.addEventListener("click", handler);
--- front/route/index.js
+++ front/route/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("layout/listForm.html");
+  res.render("layout/writeForm.html");
 });
 
 module.exports = router;
