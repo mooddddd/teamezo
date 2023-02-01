4499416186cf3dd25ca10cb65a7efb5d5e9580ab
hongtae <ambious12@gmail.com>
2023-02-01 15:30:57 +0900

    feat: adminUser.html 구성 완료

--- front/route/index.js
+++ front/route/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('admin/adminUserEdit.html')
+    res.render('admin/adminUser.html')
 })
 
 module.exports = router
