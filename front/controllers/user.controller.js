b31d2559b217a5165665248536160e94ffc46ed4
hongtae <ambious12@gmail.com>
2023-02-07 12:45:56 +0900

    feat: login / join token값 확인완료

--- front/controllers/user.controller.js
+++ front/controllers/user.controller.js
@@ -1,7 +1,53 @@
+const axios = require('axios')
+
 exports.getlogin = (req, res) => {
-    res.render("user/login.html");
-};
+    res.render('user/login.html')
+}
+
+exports.postlogin = async (req, res) => {
+    const { userid, userpw } = req.body
+    // const result = await axios.post('http://localhost:3005/user/login', {
+    //     userid,
+    //     userpw,
+    // })
+
+    const result = {
+        token: 'asd',
+        user: {
+            userid: req.body.userid,
+            username: req.body.userpw,
+        },
+    }
+
+    const cookies = JSON.stringify(result.token)
+    res.setHeader('Set-Cookie', `token=${cookies}; path=/;`)
+    res.redirect('/')
+}
 
 exports.getjoin = (req, res) => {
-    res.render("user/join.html");
-};
+    res.render('user/join.html')
+}
+
+exports.postjoin = (req, res) => {
+    const { userid, userpw, userpwchk, username, useremail } = req.body
+    const userinfo = {
+        token: 'hohoho',
+        userid,
+        userpw,
+        userpwchk,
+        username,
+        useremail,
+    }
+
+    const cookies = JSON.stringify(userinfo.token)
+    res.setHeader('Set-Cookie', `token=${cookies}; path=/;`)
+    res.redirect('/welcome')
+}
+
+exports.getwelcome = (req, res) => {
+    res.render('user/welcome.html')
+}
+
+exports.getprofile = (req, res) => {
+    res.render('user/profile.html')
+}
--- front/routes/index.js
+++ front/routes/index.js
@@ -1,15 +1,47 @@
-const express = require("express");
-const router = express.Router();
-const user = require("./user.router");
-const board = require("./board.router");
-const notice = require("./notice.router");
-const qna = require("./qna.router");
-const main = require("./router");
+const express = require('express')
+const router = express.Router()
+const user = require('./user.router')
+const board = require('./board.router')
+const notice = require('./notice.router')
+const qna = require('./qna.router')
+const main = require('./router')
 
-router.use("/", main);
-router.use("/user", user);
-router.use("/board", board);
-router.use("/notice", notice);
-router.use("/qna", qna);
+router.get('/', (req, res) => {
+    if (req.cookies) {
+        const userid = 'asdf'
+        res.render('main.html', { userid })
+    }
+})
 
-module.exports = router;
+router.get('/welcome', (req, res) => {
+    console.log(req.cookies)
+    if (req.cookies) {
+        const userid = 'hahaha'
+        const userpw = 'zzz'
+        const userpwchk = 'zzz'
+        const username = 'hongtae'
+        const useremail = 'gkgk@gmail.com'
+        res.render('user/welcome.html', { userid, username })
+    }
+})
+
+router.get('/profile', (req, res) => {
+    const profileCookies = JSON.stringify(req.cookies)
+    console.log(profileCookies === {})
+    if (profileCookies !== {}) {
+        const userid = 'hahaha'
+        const username = 'hongtae'
+        const useremail = 'hongtae@gmail.com'
+        res.render('user/profile.html', { userid, username, useremail })
+    } else if (profileCookies === {}) {
+        res.render('error.html')
+    }
+})
+
+router.use('/', main)
+router.use('/user', user)
+router.use('/board', board)
+router.use('/notice', notice)
+router.use('/qna', qna)
+
+module.exports = router
