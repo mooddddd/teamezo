759c148475d3991bb0115bad8a10d08445da45cb
mood <chamesh@naver.com>
2023-02-08 15:01:07 +0900

    feat: boardWrite.js 가변데이터에 따라 카테고리 선택할 수 있도록 설정, boardList.html 카테고리&게시글 nunjucks 설정 완료

--- front/controllers/board.controller.js
+++ front/controllers/board.controller.js
@@ -1,13 +1,44 @@
 // const request = require("./axios.test");
 
 exports.getList = async (req, res) => {
-    // 게시물&카테고리 findAll 해와야 함(axios 사용) => 그럼 불러오는 함수 2개 필요
+    // 게시물&카테고리 findAll 해와야 함(axios 사용), 좋아요 갯수는 게시물이랑 같이 받아와야 함
     // const list = await request.get();
-    res.render("board/category/boardList.html");
+    try {
+        const category = [
+            { MainCategory: "IT", subName: ["css", "html", "nodejs"] },
+            { MainCategory: "food", subName: ["한식", "양식", "몰라,,,"] },
+        ];
+        const list = [
+            {
+                idx: "1",
+                subject: "제목입니다!",
+                user_userid: "mood",
+                createAt: "2023 - 01 - 31",
+                liked: "124",
+            },
+            {
+                idx: "2",
+                subject: "제목입니다!1222",
+                user_userid: "mood222",
+                createAt: "2023 - 01 - 31",
+                liked: "14",
+            },
+        ];
+        res.render("board/category/boardList.html", { list, category });
+    } catch (e) {
+        throw new Error(e);
+    }
 };
 
 exports.getWrite = (req, res) => {
-    res.render("board/category/boardWrite.html");
+    const category = [
+        { MainCategory: "IT", subName: ["css", "html", "nodejs"] },
+        { MainCategory: "찬수", subName: ["1", "2", "3"] },
+        { MainCategory: "홍태", subName: ["하나", "둘", "셋"] },
+        { MainCategory: "은지", subName: ["원", "투", "쓰리"] },
+        // { MainCategory: "food", subName: ["한식", "양식", "몰라,,,"] },
+    ];
+    res.render("board/category/boardWrite.html", { category });
 };
 
 exports.getView = (req, res) => {
--- front/public/js/board/category/boardWrite.js
+++ front/public/js/board/category/boardWrite.js
@@ -1,28 +1,59 @@
-const categoryChange = (e) => {
-    const subCategotyIT = ["javascript", "nodejs", "html", "css"];
-    const subCategotyFood = ["한식", "중식", "일식", "양식", "기타"];
-    const subCategotyETC = ["text1", "text2"];
-    const target = document.querySelector(".subCategory");
-    const category = e.value;
-    target.options.length = 1;
+// const categoryChange = (e) => {
+//     const subCategotyIT = ["javascript", "nodejs", "html", "css"]; // 서브카테고리를 불러오기
+//     const subCategotyFood = ["한식", "중식", "일식", "양식", "기타"];
+//     const subCategotyETC = ["text1", "text2"];
+//     const target = document.querySelector(".subCategory");
+//     const category = e.value;
+//     target.options.length = 1;
 
-    if (category === "IT") {
-        for (let i = 0; i <= subCategotyIT.length - 1; i++) {
-            const subOpt = document.createElement("option");
-            subOpt.value = subCategotyIT[i];
-            subOpt.innerHTML = subCategotyIT[i];
-            target.append(subOpt);
-        }
-    } else if (category === "food") {
-        for (let i = 0; i <= subCategotyFood.length - 1; i++) {
-            const subOpt = document.createElement("option");
-            subOpt.className = "food";
-            subOpt.value = subCategotyFood[i];
-            subOpt.innerHTML = subCategotyFood[i];
-            target.append(subOpt);
+//     if (category === "IT") {
+//         for (let i = 0; i <= subCategotyIT.length - 1; i++) {
+//             const subOpt = document.createElement("option");
+//             subOpt.value = subCategotyIT[i];
+//             subOpt.innerHTML = subCategotyIT[i];
+//             target.append(subOpt);
+//         }
+//     } else if (category === "food") {
+//         for (let i = 0; i <= subCategotyFood.length - 1; i++) {
+//             const subOpt = document.createElement("option");
+//             subOpt.className = "food";
+//             subOpt.value = subCategotyFood[i];
+//             subOpt.innerHTML = subCategotyFood[i];
+//             target.append(subOpt);
+//         }
+//     }
+// };
+
+const mainCategoryBox = document.querySelector(".mainCategorySelect");
+const MainCategory = document.querySelectorAll(".mainCategorySelect > .mainCategory");
+// const holeSubCategory = document.querySelectorAll(".subject > input");
+const subCategory = document.querySelector(".subCategory");
+
+const categoryArr = Object.values(MainCategory);
+
+// const holeSubCategoryArr = Object.values(holeSubCategory); //[]
+// console.log(Object.values(MainCategory)); // [{옵션1}, {옵션2}, {옵션3}]
+// console.log(holeSubCategoryArr);
+
+mainCategoryBox.addEventListener("change", (e) => {
+    // console.log(e.target.value);
+    subCategory.options.length = 1;
+    for (i = 0; i <= MainCategory.length - 1; i++) {
+        // console.log(e.target.value);
+        if (MainCategory[i].value === e.target.value) {
+            const selectSubCategory = document.querySelectorAll(`.subject > .${MainCategory[i].value}`);
+            const selectSubCategoryArr = Object.values(selectSubCategory);
+            for (j = 0; j <= selectSubCategoryArr.length - 1; j++) {
+                // console.log(selectSubCategoryArr[j].value);
+                const subOpt = document.createElement("option");
+                subOpt.value = `${selectSubCategoryArr[j].value}`;
+                subOpt.innerHTML = `${selectSubCategoryArr[j].value}`;
+                subCategory.append(subOpt);
+            }
+            // console.log(document.getElementsByClassName(`${MainCategory[i].value}`));
         }
     }
-};
+});
 
 // 처음부터 인풋박스인 경우! 여기에 CSS 먹여서 innerHTML 처리 시키기?
 const hashtagLi = document.querySelector(".hashtagLi");
@@ -101,3 +132,54 @@ const submitHandler = async (e) => {
     // location.href = "/board/view";
 };
 writeDataForm.addEventListener("submit", submitHandler);
+
+//
+//
+//쓰레기통
+/**
+<!-- <script>
+const categoryChange = (e) => {
+{% for item in category %}
+{% set subCate = item.subName %}
+    // let subCategoty{{item.MainCategory}} = []
+    let arr = []
+arr.push({{subCate}})
+// for(let i=0; i<pushsub.length; i++){
+//     arr.push(`${pushsub[i]}`)
+// }
+    // const subCategotyFood = ["한식", "중식", "일식", "양식", "기타"];
+    // const subCategotyETC = ["text1", "text2"];
+    let target = document.querySelector(".subCategory");
+    let category = "{{item.MainCategory}}";
+    {% endfor %}
+    target.options.length = 1;
+
+    // if (category === "IT") {
+    //     for (let i = 0; i <= subCategotyIT.length - 1; i++) {
+    //         const subOpt = document.createElement("option");
+    //         subOpt.value = subCategotyIT[i];
+    //         subOpt.innerHTML = subCategotyIT[i];
+    //         target.append(subOpt);
+    //     }
+    // }
+    // } else if (category === "food") {
+    //     for (let i = 0; i <= subCategotyIT.length - 1; i++) {
+    //         const subOpt = document.createElement("option");
+    //         subOpt.className = "food";
+    //         subOpt.value = subCategotyIT[i];
+    //         subOpt.innerHTML = subCategotyIT[i];
+    //         target.append(subOpt);
+    //     }
+    // }
+    // } else if (category === "food") {
+    //     for (let i = 0; i <= subCategotyFood.length - 1; i++) {
+    //         const subOpt = document.createElement("option");
+    //         subOpt.className = "food";
+    //         subOpt.value = subCategotyFood[i];
+    //         subOpt.innerHTML = subCategotyFood[i];
+    //         target.append(subOpt);
+    //     }
+    // }
+};
+</script> -->
+ */
