6e97ecc68bc5f50f9b5870524aa90fa6f8a6caf3
hongtae <ambious12@gmail.com>
2023-02-13 12:11:37 +0900

    feat: profile 출력

--- front/controllers/user.controller.js
+++ front/controllers/user.controller.js
@@ -47,66 +47,7 @@ exports.getprofile = (req, res) => {
     if (profileCookies === undefined) {
         res.render('error.html')
     } else {
-        const result = [
-            {
-                idx: 0,
-                userid: 'hongtae',
-                username: 'hongtae',
-                useremail: 'asd@naver.com',
-                subject: '게시글 제목임',
-                comment: 'hongtae111',
-                hit: '3',
-            },
-            {
-                idx: 1,
-                userid: 'hongtae2',
-                username: 'hongtae2',
-                useremail: 'asd2@naver.com',
-                subject: '게시글 제목임2',
-                comment: 'hongtae222',
-                hit: '23',
-            },
-            {
-                idx: 2,
-                userid: 'hongtae3',
-                username: 'hongtae3',
-                useremail: 'asd3@naver.com',
-                subject: '게시글 제목임3',
-                comment: 'hongtae333',
-                hit: '4',
-            },
-            {
-                idx: 3,
-                userid: 'hongtae4',
-                username: 'hongtae4',
-                useremail: 'asd4@naver.com',
-                subject: '게시글 제목임4',
-                comment: 'hongtae444',
-                hit: '55',
-            },
-            {
-                idx: 4,
-                userid: 'hongtae5',
-                username: 'hongtae5',
-                useremail: 'asd5@naver.com',
-                subject: '게시글 제목임5',
-                comment: 'hongtae555',
-                hit: '21',
-            },
-            {
-                idx: 5,
-                userid: 'hongtae6',
-                username: 'hongtae6',
-                useremail: 'asd6@naver.com',
-                subject: '게시글 제목임6',
-                comment: 'hongtae666',
-                hit: '66',
-            },
-        ]
-
-        const resultLength = result.length
-        const totalPage = Math.ceil(resultLength / 5)
-        res.render('user/profile.html', { result, resultLength, totalPage })
+        res.render('user/profile.html')
     }
 }
 
